231ab5b480cafdd8f63c1cd05f5893f9b7367b0b
kw19657 <kenscopy@gmail.com>
2022-06-09 14:55:21 +0800

    final

--- script.js
+++ script.js
@@ -1,68 +1,39 @@
-// for (let i = 0; i < difficulty; i++) {
-//   let firstNumber = Math.random() * difficulty
-//   let secondNumber = math random
-//   let question = `${firstNumber}` + `${secondNumber}`
-//   let annwer = firstNumber + secondNumber
-//   questionAr.push(qestion)
-//   answerArr.push(answer)
-// }
-
-let eggNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let typing = "";
 let windowWidth = window.innerWidth;
-let body = document.body;
 let windowHeight = window.innerHeight;
 let score = 0;
-let gameLives = 3;
+let gameLives;
 let gameOver = false;
 let endGame = document.querySelector(".end-game");
-// let allEggs = document.querySelectorAll(".egg");
+let inputBox = document.getElementById("input-box");
 
 function createEgg() {
   let div = document.createElement("div");
   div.className = "egg";
-  div.innerText = eggNumber[Math.floor(Math.random() * eggNumber.length)];
-  // div.setAttribute("answer", 9)
+  let randspd = Math.ceil(Math.random() * 10) + 2;
+  div.style.animationDuration = randspd + "s";
+  let rand1 = Math.ceil(Math.random() * 15);
+  let rand2 = Math.ceil(Math.random() * 15);
+  let answer = rand1 + rand2;
+  randNum = Math.ceil(Math.random() * 9);
+  div.setAttribute("answer", answer);
+  div.innerText = `${rand1}+${rand2}`;
 
   let rand = Math.floor(Math.random() * (windowWidth - 101));
   div.style.left = rand + "px";
 
-  function eggDrop(elem) {
-    let id = null;
-    let pos = 0;
-    let randSpeed = Math.floor(Math.random() * 25);
-
-    clearInterval(id);
-
-    id = setInterval(frame, randSpeed);
-
-    function frame() {
-      if (pos === windowHeight - 101) {
-        clearInterval(id);
-        elem.innerText = "dead";
-        gameLives--;
-        document.querySelector(".lives").innerText = gameLives;
-        if (gameLives === 0) {
-          gameOver = true;
-        }
-      } else {
-        pos++;
-        elem.style.top = pos + "px";
-      }
+  div.addEventListener("animationend", function (e) {
+    e.target.style.top = windowHeight - 101 + "px";
+    e.target.setAttribute("answer", "dead");
+    gameLives--;
+    document.querySelector(".lives").innerText = gameLives;
+    if (gameLives === 0) {
+      gameOver = true;
     }
-
-    document.addEventListener("keydown", function (e) {
-      if (e.key === div.innerText) {
-        div.innerText = "saved";
-        div.remove();
-        clearInterval(id);
-        score++;
-        document.querySelector(".score").innerText = score;
-      }
-    });
-  }
+    e.target.innerText = "dead";
+  });
 
   document.body.appendChild(div);
-  eggDrop(div);
 }
 
 function restartGame() {
@@ -73,7 +44,7 @@ function restartGame() {
   }
   score = 0;
   document.querySelector(".score").innerText = score;
-  gameLives = 3;
+  gameLives = 5;
   document.querySelector(".lives").innerText = gameLives;
 }
 
@@ -84,9 +55,8 @@ document.querySelector(".start-btn").addEventListener("click", function (e) {
 
 document.querySelector(".restart-btn").addEventListener("click", function (e) {
   startGame();
-  // document.querySelector(".end-game").style.display = "none";
-  // document.querySelector(".score-board").style.display = "flex";
-  location.reload();
+  document.querySelector(".end-game").style.display = "none";
+  document.querySelector(".score-board").style.display = "flex";
 });
 
 function startGame() {
@@ -105,3 +75,43 @@ function startGame() {
     }
   }, 800 + timeout);
 }
+
+// Keyboard inputs
+document.addEventListener("keydown", function (e) {
+  let numerical = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "Backspace",
+    "Enter",
+  ];
+
+  if (numerical.includes(e.key)) {
+    if (e.key === "Backspace") {
+      let temp = typing.split("");
+      temp.pop();
+      typing = temp.join("");
+    } else if (e.key === "Enter") {
+      let eggCheckerAll = document.querySelectorAll(".egg");
+      eggCheckerAll.forEach(function (egg) {
+        let answerCheck = egg.getAttribute("answer");
+        if (answerCheck === typing) {
+          egg.remove();
+          score++;
+          document.querySelector(".score").innerText = score;
+        }
+      });
+      typing = "";
+    } else {
+      typing += e.key;
+    }
+  }
+  document.querySelector("#input-box").innerText = typing;
+});
